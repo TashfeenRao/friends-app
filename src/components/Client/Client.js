0eb1094eaf6ccd108bae771933db655e8ad2dab4
tashfeenrao <raotashfeen25@gmail.com>
2020-11-12 00:00:41 +0500

    filter method apply to api for clinets and providers

--- src/components/Client/Client.js
+++ src/components/Client/Client.js
@@ -1,47 +1,8 @@
-import { CircularProgress, Grid } from "@material-ui/core";
-import Axios from "axios";
-import { useEffect, useState } from "react";
+import { Grid } from "@material-ui/core";
 import AppBarComp from "../Provider/AppBar";
 import CardComp from "../Provider/CardComp";
-import toastError from "../toast/toastError";
 
-export default function Client() {
-  const [loading, setLoading] = useState(false);
-  const [users, setUsers] = useState(null);
-
-  useEffect(() => {
-    const config = {
-      method: "get",
-      url: "http://localhost:1337/users",
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem("token")}`,
-      },
-    };
-    setLoading(true);
-    Axios(config)
-      .then((response) => {
-        setUsers(response.data);
-        setLoading(false);
-      })
-      .catch((error) => {
-        toastError("could not able to find users");
-        setLoading(false);
-      });
-  }, []);
-
-  if (loading)
-    return (
-      <CircularProgress
-        color="secondary"
-        size={100}
-        style={{
-          position: "absolute",
-          top: "50%",
-          left: "45%",
-        }}
-      />
-    );
-
+export default function Client({ filUsers }) {
   return (
     <Grid container spacing={1}>
       <Grid item xs={12}>
@@ -50,8 +11,8 @@ export default function Client() {
       <Grid item xs={12}>
         <h1>All Clients</h1>
       </Grid>
-      {users &&
-        users.map((user) => (
+      {filUsers &&
+        filUsers.map((user) => (
           <Grid key={user.id} item xs={12} sm={4}>
             <CardComp
               username={user.username}
--- src/components/Root/MainPage.js
+++ src/components/Root/MainPage.js
@@ -1,10 +1,53 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Provider from "../Provider/Provider";
 import { userContext } from "../../App";
 import Client from "../Client/Client";
+import { CircularProgress } from "@material-ui/core";
+import toastError from "../toast/toastError";
+import Axios from "axios";
 
 export default function MainPage() {
   const { user } = useContext(userContext);
   const { role } = user;
-  return role.name === "provider" ? <Provider /> : <Client />;
+  const [loading, setLoading] = useState(false);
+  const [users, setUsers] = useState(null);
+
+  useEffect(() => {
+    const config = {
+      method: "get",
+      url: "http://localhost:1337/users",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    };
+    setLoading(true);
+    Axios(config)
+      .then((response) => {
+        setUsers(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        toastError("could not able to find users");
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading)
+    return (
+      <CircularProgress
+        color="secondary"
+        size={100}
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "45%",
+        }}
+      />
+    );
+  const filUsers = users && users.filter((c) => c.role.name === `${role.name}`);
+  return role.name === "provider" ? (
+    <Provider filUsers={filUsers} />
+  ) : (
+    <Client filUsers={filUsers} />
+  );
 }
